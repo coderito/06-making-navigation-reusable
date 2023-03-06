f1f2bcf2cfcf3e365f76d36e1e40fd326e3be8d4
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-06 12:01:32 -0300

    Customizing the Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom'
 
-export const Modal = ({onClose}) => {
+export const Modal = ({ onClose, children, actionBar }) => {
   return ReactDOM.createPortal(
     <div>
       <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-30"></div>
-      <div className="absolute inset-40 p-10 bg-white">Modal</div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
     </div>,
     document.querySelector(".modal-container")
   )
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -13,14 +13,22 @@ export const ModalPage = () => {
     setShowModal(false)
   }
 
+  const actionBar = <div>
+    <Button onClick={handleClose} primary>I Accept</Button>
+  </div>
+
+  const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+    <p>lorem ipsun bla bla bla</p>
+  </Modal>
+
   return (
     <div>
-        <Button 
-          className="rounded-md hover:bg-blue-600"
-          onClick={handleClick}
-          primary 
-        >Show Modal</Button>
-        {showModal && <Modal onClose={handleClose}/>}
+      <Button
+        className="rounded-md hover:bg-blue-600"
+        onClick={handleClick}
+        primary
+      >Show Modal</Button>
+      {showModal && modal}
     </div>
   )
 }
