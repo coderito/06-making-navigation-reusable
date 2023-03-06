826a77dcf3db86647efaa9a544202f3da01eab86
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-06 11:43:25 -0300

    Fixing the modal with Portals

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import ReactDOM from 'react-dom'
 
 export const Modal = () => {
-  return (
-    <div>Modal !!!</div>
+  return ReactDOM.createPortal(
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-30"></div>
+      <div className="absolute inset-40 p-10 bg-white">Modal</div>
+    </div>,
+    document.querySelector(".modal-container")
   )
 }
